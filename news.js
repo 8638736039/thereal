8ca95ac71d253bcf3ee55fb76bc2884f7e1cfeba
8638736039 <baruahpankaj2@gmail.com>
2025-07-01 18:08:30 +0530

    Add files via upload

---
+++ news.js
@@ -0,0 +1,6 @@
+document.getElementById('news-container').innerHTML = `
+  <article>
+    <h2>Sample News Headline</h2>
+    <p>This is a sample news article content loaded from JavaScript.</p>
+  </article>
+`;
